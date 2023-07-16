1c92cd619ca795de7368c80b183046ca58be2198
dnedzelskyi <dimanedzelskyi@gmail.com>
2023-07-16 11:15:17 -0700

    #2 [Technical] Add ESLint. Fix gitignore to allow root js config files.

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  root: true,
+  rules: {
+    // Custom rules or overrides go here
+  },
+};
