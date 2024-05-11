0bb5c5161db7052be72237230260743f47de3876
dnedzelskyi <dimanedzelskyi@gmail.com>
2024-05-10 23:00:01 -0700

    #15 [Infrastructure] Set up Unit Testing for the project.

---
+++ __tests__/services/storage.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from '@jest/globals';
+import { StorageKey, StorageService } from '../../src/services/storage';
+
+describe('StorageService Test Suite.', () => {
+  test('Should be able to store and retrieve data.', () => {
+    const testText = 'test note';
+
+    const storage = new StorageService(window.localStorage);
+    storage.write(StorageKey.NOTES_STORAGE_KEY, testText);
+
+    expect(storage.read(StorageKey.NOTES_STORAGE_KEY)).toBe(testText);
+  });
+});
